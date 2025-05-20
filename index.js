5839482f65388325177742c97575bdc9e37f5668
Santhosh <santhoshsrinivasanofficial@gmail.com>
2025-05-20 12:37:01 +0100

    update with array, and objects

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const name ='santhosh'
 const intrestRate=0.3;
  console.log(intrestRate);
 
+let person = {name: 'santhosh',
+    age:26
+};
+console.log(person)
+person.name = 'santhosh srinivasan';
+person['age'] = 27;
+
+console.log(person.name);
+console.log(person.age);
+
+//js arrays
+let selectedColors=['red','blue'];
+selectedColors[2]='10';
+console.log(selectedColors[1]);
+console.log(selectedColors);
