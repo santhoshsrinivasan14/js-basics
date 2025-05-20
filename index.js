ccef00592bc20c9d8be1f7d471c7c697eb65c1b1
Santhosh <santhoshsrinivasanofficial@gmail.com>
2025-05-20 13:10:20 +0100

    funtions

--- index.js
+++ index.js
@@ -16,4 +16,21 @@ console.log(person.age);
 let selectedColors=['red','blue'];
 selectedColors[2]='10';
 console.log(selectedColors[1]);
+console.log(selectedColors[2]);
 console.log(selectedColors);
+
+//functions
+function greet(name,lastName) {
+    console.log('Hello ' + name + ' '+lastName);
+}
+
+greet('santhosh','srinivasan');
+
+//calculating area of circle
+function calculatedArea(radius) {
+    return 3.14 * radius * radius;
+}
+
+let rad = calculatedArea(24);
+console.log(rad)
+console.log(calculatedArea(10));
